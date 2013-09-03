73a8f2885284caae56d893a9b86cc5cb51bbe309
Usuario <Usuario@Jazmin>
2013-09-02 21:10:48 -0600

    clases de abogados

--- SCFEJ/src/java/com/Despachos/Abogados/AbogadosBean.java
+++ SCFEJ/src/java/com/Despachos/Abogados/AbogadosBean.java
@@ -6,18 +6,145 @@ package com.Despachos.Abogados;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import Entidades.Abogados;
+import java.sql.SQLException;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 
 /**
  *
- * @author Usuario
+ * @author Tellin
  */
 @ManagedBean
 @RequestScoped
 public class AbogadosBean {
 
-    /**
-     * Creates a new instance of AbogadosBean
-     */
-    public AbogadosBean() {
+    private int idAbogado;
+    private String nombre;
+    private String primerApellido;
+    private String segundoApellido;
+    private String noCarne;
+    private String cedula;
+    private String direccion;
+    private int telefono;
+    private int celular;
+    private String email;
+    private Abogados entidad;
+    
+    public AbogadosBean() {       
+        entidad = new Abogados();
     }
+
+    public Integer getidAbogado(){
+        return idAbogado;
+    }
+   
+    public void setidAbogado(Integer idAbogado) {
+        this.idAbogado = idAbogado;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPrimerApellido() {
+        return primerApellido;
+    }
+
+    public void setPrimerApellido(String primerApellido) {
+        this.primerApellido = primerApellido;
+    }
+
+    public String getSegundoApellido() {
+        return segundoApellido;
+    }
+
+    public void setSegundoApellido(String segundoApellido) {
+        this.segundoApellido = segundoApellido;
+    }
+    
+    public String getNoCarne(){
+        return noCarne;
+    }
+
+    public void setNoCarne(String noCarne){
+        this.noCarne = noCarne;
+    }
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public int getCelular() {
+        return celular;
+    }
+
+    public void setCelular(int celular) {
+        this.celular = celular;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public void guardarAbogados() throws SQLException{
+    
+        entidad.setIdAbogado(idAbogado);
+        entidad.setNombre(nombre);
+        entidad.setDireccion(direccion);
+        entidad.setPrimerApellido(primerApellido);
+        entidad.setSegundoApellido(segundoApellido);
+        entidad.setNoCarne(noCarne);
+        entidad.setCedula(cedula);
+        entidad.setCelular(celular);
+        entidad.setTelefono(telefono);
+        entidad.setEmail(email);
+        
+        entidad.guardarAbogados(entidad);
+               
+    }
+    
+    public void msg() {
+        boolean comprobar=true;
+        try{
+            this.guardarAbogados();
+        }
+        catch(SQLException ex){
+            FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR,"Almacenamiento de Datos","Error al insertar los Datos");
+            FacesContext.getCurrentInstance().addMessage(null, msg);
+            comprobar=false;
+        }
+        if(comprobar==true){
+            FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO,"Almacenamiento de Datos","Datos guardados exitosamente");
+            FacesContext.getCurrentInstance().addMessage(null, msg);
+        }  
+    }
+    
 }
--- SCFEJ/src/java/com/Despachos/Abogados/AbogadosDAO.java
+++ SCFEJ/src/java/com/Despachos/Abogados/AbogadosDAO.java
@@ -1,13 +1,29 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.Despachos.Abogados;
 
-/**
- *
- * @author Usuario
- */
+import Conexion.ConexionDB;
+import Entidades.Abogados;
+import com.mysql.jdbc.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class AbogadosDAO {
+    
+    public AbogadosDAO() {
+        
+    }
+    
+     public void guardarAbogados(Abogados entidad) throws SQLException{
+    
+        Connection conexion = new ConexionDB().conectar();
+        String query = "insert into Abogados values("+entidad.getIdAbogado()+",'"+entidad.getNombre()+"','"
+                        +entidad.getPrimerApellido()+"','"+entidad.getSegundoApellido()+"','"+entidad.getNoCarne()+"','"
+                        +entidad.getCedula()+"','"+entidad.getDireccion()+"',"+entidad.getTelefono()+","
+                        +entidad.getCelular()+",'"+entidad.getEmail()+"');";
+        PreparedStatement exec = conexion.prepareStatement(query);
+        exec.executeUpdate();
+        conexion.close();
+        
+    }
+    
     
 }
