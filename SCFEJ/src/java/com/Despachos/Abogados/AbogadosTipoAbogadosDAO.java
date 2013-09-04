82b744d9b71f99c4522dd6c1313c0fa6702c7a5b
Usuario <Usuario@Jazmin>
2013-09-03 22:13:48 -0600

    clases de abogados

--- SCFEJ/src/java/com/Despachos/Abogados/AbogadosTipoAbogadosDAO.java
+++ SCFEJ/src/java/com/Despachos/Abogados/AbogadosTipoAbogadosDAO.java
@@ -18,7 +18,7 @@ public class AbogadosTipoAbogadosDAO {
     public void guardarTipoAbogado(AbogadoTipoabogado entidad) throws SQLException{
     
         Connection conexion = new ConexionDB().conectar();
-        String query = "insert into abogado_tipoabogado values((select max(idAbogado) FROM abogados),"+entidad.getIdtipoabogado()+","+entidad.isActivo()+");";
+        String query = "insert into despachoabogados.abogado_tipoabogado values("+entidad.getIdtipoabogado()+",(select max(idAbogado) FROM despachoabogados.abogados),"+entidad.isActivo()+");";
         PreparedStatement exec = conexion.prepareStatement(query);
         exec.executeUpdate();
         conexion.close();
